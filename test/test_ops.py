a790301d311da9f032b3302d8e64ba135288e554
Nikita Karetnikov <nikita@karetnikov.org>
2022-08-26 17:53:50 +0200

    [primTorch] Add docstring and promotion for `l1_loss` ref
    ghstack-source-id: 321c40a4a53b13fe29f51b3e0c0bd2dbd54f8ddc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83803

--- test/test_ops.py
+++ test/test_ops.py
@@ -1615,6 +1615,7 @@ class TestRefsOpsInfo(TestCase):
         '_refs.isfinite',
         '_refs.movedim',
         '_refs.narrow',
+        '_refs.nn.functional.l1_loss',
         '_refs.positive',
         '_refs.ravel',
         '_refs.reshape',
