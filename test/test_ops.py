2a78e9c0156d1829c1d28cfbb8888ee44edca567
Nikita Karetnikov <nikita@karetnikov.org>
2022-08-26 17:53:51 +0200

    [primTorch] Add ref for `poisson_nll_loss`
    ghstack-source-id: 77797b600e0b0d87eb868e4e29225ba0a7e0fab2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83805

--- test/test_ops.py
+++ test/test_ops.py
@@ -1616,6 +1616,7 @@ class TestRefsOpsInfo(TestCase):
         '_refs.movedim',
         '_refs.narrow',
         '_refs.nn.functional.l1_loss',
+        '_refs.nn.functional.poisson_nll_loss',
         '_refs.positive',
         '_refs.ravel',
         '_refs.reshape',
